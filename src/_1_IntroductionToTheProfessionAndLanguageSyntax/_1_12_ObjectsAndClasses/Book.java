47856b6e41218788df1aad58857c4168ba520cd8
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-06-28 08:11:56 +0300

    1.00

---
+++ src/_1_IntroductionToTheProfessionAndLanguageSyntax/_1_12_ObjectsAndClasses/Book.java
@@ -0,0 +1,36 @@
+package _1_IntroductionToTheProfessionAndLanguageSyntax._1_12_ObjectsAndClasses;
+
+public class Book {
+    private String title;
+    private Author author;
+    private int publicYear;
+
+    public Book(String title, Author author, int publicYear) {
+        this.title = title;
+        this.author = author;
+        this.publicYear = publicYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicYear() {
+        return publicYear;
+    }
+
+    public void setPublicYear(int publicYear) {
+        this.publicYear = publicYear;
+    }
+
+    //////////////////////////////////////////////////
+    public void printBook() {
+        System.out.println("Название книги: " + this.title);
+        this.author.printAuthor();
+        System.out.println("Год издания: " + this.publicYear);
+    }
+}
