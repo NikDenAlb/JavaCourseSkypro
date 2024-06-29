8dc90500bee44b4be60918c32dc3d15bd983cdd8
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-06-29 15:49:29 +0300

    1.00

---
+++ src/_1_IntroductionToTheProfessionAndLanguageSyntax/_1_13_ObjectMethods/Author.java
@@ -0,0 +1,43 @@
+package _1_IntroductionToTheProfessionAndLanguageSyntax._1_13_ObjectMethods;
+
+import java.util.Objects;
+
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void printAuthor() {
+        System.out.println("Автор: " + getName() + " " + getSurname());
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " " + getSurname();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(getName(), author.getName()) && Objects.equals(getSurname(), author.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname());
+    }
+}
---
+++ src/_1_IntroductionToTheProfessionAndLanguageSyntax/_1_13_ObjectMethods/Book.java
@@ -0,0 +1,57 @@
+package _1_IntroductionToTheProfessionAndLanguageSyntax._1_13_ObjectMethods;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private Author author;
+    private int publicYear;
+
+    public Book(String title, Author author, int publicYear) {
+        this.title = title;
+        this.author = author;
+        this.publicYear = publicYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicYear() {
+        return publicYear;
+    }
+
+    public void setPublicYear(int publicYear) {
+        this.publicYear = publicYear;
+    }
+
+    public void printBook() {
+        System.out.println("Название книги: " + this.title);
+        this.author.printAuthor();
+        System.out.println("Год издания: " + this.publicYear);
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + getTitle() + "\" " +
+                getAuthor()
+                + " " + getPublicYear() + "г.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getTitle(), book.getTitle()) && Objects.equals(getAuthor(), book.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getAuthor());
+    }
+}
