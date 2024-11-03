77af25ffa86b1cce422b57cc19478f9afbf32d3e
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-11-03 18:14:50 +0300

    1.00

---
+++ src/_2_JavaCore/_2_7_SetsAndMaps/Main.java
@@ -0,0 +1,81 @@
+package _2_JavaCore._2_7_SetsAndMaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
+        printOdd(nums);
+    }
+
+    private static void printOdd(List<Integer> listInteger) {
+        for (Integer i : listInteger) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("\nЗадача 2");
+        List<Integer> nums = new ArrayList<>(List.of(10, 11, 12, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
+        printEven(nums);
+    }
+
+    private static void printEven(List<Integer> listInteger) {
+        SortedSet<Integer> set = new TreeSet<>(listInteger);
+        for (Integer i : set) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("\nЗадача 3");
+        List<String> words = new ArrayList<>(List.of("слово1", "слово1", "слово3", "слово4", "слово5", "слово10", "слово3", "слово2", "слово4", "слово4"));
+        System.out.println(words);
+        printDiffWords(words);
+    }
+
+    private static void printDiffWords(List<String> listString) {
+        Set<String> set = new TreeSet<>(listString);
+        for (String i : set) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task4() {
+        System.out.println("\nЗадача 4");
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        System.out.println(strings);
+        System.out.println(wordNumberMap(strings));
+    }
+
+    private static Map<String, Integer> wordNumberMap(List<String> listString) {
+        Map<String, Integer> wordNumberMap = new HashMap<>();
+        for (String i : listString) {
+            wordNumberMap.merge(i, 1, Integer::sum);
+        }
+        return wordNumberMap;
+    }
+}
