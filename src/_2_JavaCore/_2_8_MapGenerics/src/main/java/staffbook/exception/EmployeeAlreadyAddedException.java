97fd37885d817953e8aa44616ea7b37745227b1a
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-11-04 20:21:55 +0300

    2_8 fish

---
+++ src/_2_JavaCore/_2_8_MapGenerics/src/main/java/staffbook/exception/EmployeeAlreadyAddedException.java
@@ -0,0 +1,7 @@
+package staffbook.exception;
+
+public class EmployeeAlreadyAddedException extends RuntimeException {
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+}
