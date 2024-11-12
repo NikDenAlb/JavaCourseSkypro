e5403b68cfe805a82d7cc75c0e16e9fcea86577c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-11-12 19:08:35 +0300

    Bicycle->ServiceVehicle

--- src/_2_JavaCore/_2_3_OOP2/ServiceStation.java
+++ src/_2_JavaCore/_2_3_OOP2/ServiceStation.java
@@ -2,7 +2,7 @@ package _2_JavaCore._2_3_OOP2;
 
 public class ServiceStation {
 
-    public void check(Bicycle vehicle) {
+    public void check(ServiceVehicle vehicle) {
         System.out.println("Обслуживаем " + vehicle.getModelName());
         vehicle.service();
     }
