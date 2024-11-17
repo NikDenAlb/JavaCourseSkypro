1ae160d3e9404788f6edea62e5d7e71eeae3621f
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-11-17 07:54:59 +0300

    2_15 fish

---
+++ src/_2_JavaCore/_2_15_AlgorithmsPartTwo/main/exception/InvalidIndexException.java
@@ -0,0 +1,6 @@
+package _2_JavaCore._2_15_AlgorithmsPartTwo.main.exception;
+
+public class InvalidIndexException extends IllegalArgumentException {
+    public InvalidIndexException() {
+    }
+}
---
+++ src/_2_JavaCore/_2_15_AlgorithmsPartTwo/main/exception/StorageIsFullException.java
@@ -0,0 +1,6 @@
+package _2_JavaCore._2_15_AlgorithmsPartTwo.main.exception;
+
+public class StorageIsFullException extends RuntimeException {
+    public StorageIsFullException() {
+    }
+}
