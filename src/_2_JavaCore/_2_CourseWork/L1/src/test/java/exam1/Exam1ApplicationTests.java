cbc8029de6a9cbdcee0433ca2e9ae71d33df8473
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-05 12:55:20 +0300

    Initial commit

---
+++ src/_2_JavaCore/_2_CourseWork/L1/src/test/java/exam1/Exam1ApplicationTests.java
@@ -0,0 +1,12 @@
+package exam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
